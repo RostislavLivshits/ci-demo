37a52640e82d2e44b854d2b3ecb8c86cf2d78563
Slava Livshits <slava@securepush.com>
2026-03-09 04:18:59 +0200

    new branch test commit

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,6 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-   // print('Counter before increment: $_counter'); // Debug print statement
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
